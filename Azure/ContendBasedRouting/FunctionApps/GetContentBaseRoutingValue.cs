ad2e5d1e3715d5ab82b95cf565d4efce29f2e856
Anders Carlebring <anders.carlebring@solita.fi>
2023-12-13 08:15:17 +0100

    changed folder structure.

---
+++ Azure/ContendBasedRouting/FunctionApps/GetContentBaseRoutingValue.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ContentBaseRoutingFunctionApp
+{
+   
+    public class GetContentBaseRoutingValue
+    {
+        private readonly ILogger _logger;
+
+        public GetContentBaseRoutingValue(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<GetContentBaseRoutingValue>();
+        }
+
+        [FunctionName("GetContentBaseRoutingValue")]
+        public async Task<String> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function GetContentBaseRoutingValue processed a request.");
+
+            try
+            {
+
+                System.Diagnostics.Trace.WriteLine("This is my function logging from Diagnostics");
+                _logger.LogInformation("This is my function logging from ILogger");
+
+                string variableName = req.Query["RoutingValue"];
+                string routingValue = Environment.GetEnvironmentVariable(variableName);
+
+            return routingValue;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine($"Error Diagnostics: {ex.Message}");
+                _logger.LogError(ex.Message);
+                return ex.ToString();
+            }
+        }
+    }
+}
